d73e3356dbed819e7441494b7d41c46dd8b84a35
David Kolaja <dave.kolaja@gmail.com>
2024-02-07 06:22:37 +0000

    cleanup

--- next.config.js
+++ next.config.js
@@ -19,11 +19,7 @@ const nextConfig = {
   //   ],
   // },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+    // ignoreBuildErrors: true,
   },
   // output: "standalone",
 };
