afb9965b03ed3709a8ec2a8429931e27a8b77855
David Kolaja <dave.kolaja@gmail.com>
2024-02-08 17:11:24 +0000

    fix: add missing token to query

--- cms/items/legal.ts
+++ cms/items/legal.ts
@@ -14,10 +14,13 @@ export type TLegal = {
 type TLegalList = Omit<TLegal, "content">;
 
 async function previewSlug(slug: string) {
-  return await cmsAPI(`${base}?filter[slug]=${slug}`, {
-    method: "GET",
-    cache: "no-store",
-  }).then((res) => res.data[0] as TLegal);
+  return await cmsAPI(
+    `${base}?filter[slug]=${slug}&access_token=${draftToken}`,
+    {
+      method: "GET",
+      cache: "no-store",
+    }
+  ).then((res) => res.data[0] as TLegal);
 }
 
 async function readSlug(slug: string) {
