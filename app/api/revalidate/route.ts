522b34a945458c3670ed303efb2f6b719c321fde
David Kolaja <dave.kolaja@gmail.com>
2024-02-12 19:20:46 +0000

    refactor

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,27 +2,29 @@ import { revalidatePath, revalidateTag } from "next/cache";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const authToken = request.nextUrl.searchParams.get("authToken");
+  const token = request.nextUrl.searchParams.get("token");
 
-  if (authToken !== process.env.PROJECT_KEY) {
+  if (token !== process.env.PROJECT_KEY) {
     return Response.json({ message: "Invalid token" }, { status: 401 });
   }
 
-  const body = await request.json();
+  const { tags, path } = await request.json();
 
-  if (!body.tag && !body.path) {
+  if (!tags && !path) {
     return Response.json(
-      { message: "You must provide a tag or a path" },
+      { message: "You must provide tags or path" },
       { status: 400 }
     );
   }
 
-  if (body.tag) {
-    revalidateTag(body.tag);
+  if (tags) {
+    tags.forEach((tag: string) => {
+      tag && revalidateTag(tag);
+    });
   }
 
-  if (body.path) {
-    revalidatePath(body.path);
+  if (path) {
+    revalidatePath(path);
   }
 
   return Response.json({ revalidated: true, now: Date.now() });
