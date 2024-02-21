2f102546b34138194609592e4e4ea92cfcff63e9
David Kolaja <dave.kolaja@gmail.com>
2024-02-21 17:48:04 +0000

    add revalidation tag for products

--- app/api/_cms/items/gallery.ts
+++ app/api/_cms/items/gallery.ts
@@ -34,7 +34,7 @@ async function readItem(id: string) {
       method: "GET",
       cache: isDraft ? "no-store" : "default",
       next: {
-        tags: isDraft ? [] : [id],
+        tags: isDraft ? [] : ["products", id],
       },
     },
     isDraft
