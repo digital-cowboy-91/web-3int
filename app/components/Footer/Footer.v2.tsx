b8fe584e21f52530fdae51c43be78715ba804717
David Kolaja <dave.kolaja@gmail.com>
2024-04-27 12:14:55 +0100

    activate footer menu

--- app/components/Footer/Footer.v2.tsx
+++ app/components/Footer/Footer.v2.tsx
@@ -1,10 +1,30 @@
+import { notFound } from "next/navigation";
 import "./Footer.v2.style.css";
+import { CMSLegal } from "@/app/api/_cms/collections/legal";
+import Action from "../Actions/Action";
 
-export default function Footer() {
+export default async function Footer() {
+  const res = await CMSLegal.readItems();
+
+  if (!res) notFound();
+
   return (
     <footer className="footer">
       <div className="footer__wrapper">
-        <div className="footer__menu">Menu</div>
+        <div className="footer__menu">
+          <ul>
+            {res.map(({ title, slug }) => (
+              <li key={slug}>
+                <Action
+                  as="link"
+                  href={`/legal/${slug}`}
+                  variant="underscored"
+                  label={title}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className="footer__copyright">
           © 3int UK {new Date().getFullYear()} <span>|</span> developed by{" "}
           <span>digitalcowboys</span>
