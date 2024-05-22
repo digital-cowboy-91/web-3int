b75c64b015d4e804a35a9506b9cd9c419f3edb92
David Kolaja <dave.kolaja@gmail.com>
2024-05-22 17:48:06 +0100

    dump non functional faq

--- app/faq/components/FAQ.tsx
+++ app/faq/components/FAQ.tsx
@@ -4,29 +4,30 @@ import "./FAQ.style.css";
 import FAQQuestionButton from "./FAQQuestionButton";
 
 export default async function FAQ() {
-  const res = await CMSFAQ.readItems();
+  return null;
+  // const res = await CMSFAQ.readItems();
 
-  if (!res) notFound();
+  // if (!res) notFound();
 
-  return (
-    <div className="faq">
-      <h2 className="faq__title">Frequently Asked Questions</h2>
-      <div className="faq__items-wrapper">
-        {res.map(({ id, answer, question }) => (
-          <div
-            className="faq__item"
-            key={id}
-            id={"faq_" + id}
-            data-state="closed"
-          >
-            <FAQQuestionButton id={"faq_" + id} question={question} />
+  // return (
+  //   <div className="faq">
+  //     <h2 className="faq__title">Frequently Asked Questions</h2>
+  //     <div className="faq__items-wrapper">
+  //       {res.map(({ id, answer, question }) => (
+  //         <div
+  //           className="faq__item"
+  //           key={id}
+  //           id={"faq_" + id}
+  //           data-state="closed"
+  //         >
+  //           <FAQQuestionButton id={"faq_" + id} question={question} />
 
-            <div className="faq__item__answer" data-state="closed">
-              {answer}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  //           <div className="faq__item__answer" data-state="closed">
+  //             {answer}
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
